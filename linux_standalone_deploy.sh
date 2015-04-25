84ab7e9908513bbe3b0de0b6fd71a028160917d2
Yanjie Gao <396154235@qq.com>
2015-04-25 20:30:47 +0800

    Create linux_standalone_deploy.sh

---
+++ linux_standalone_deploy.sh
@@ -0,0 +1,21 @@
+# 1 安装jdk
+# http://www.oracle.com/technetwork/java/javase/downloads/index.html
+
+# 2 下载Spark bin
+# http://spark.apache.org/downloads.html
+
+# 3 tar -xzvf
+
+# 4 配置conf/spark-env.sh
+
+# 5 配置conf/slaves
+
+# 6 下载pssh
+# https://github.com/knktc/insecure_pssh
+
+# 7 拷贝Spark到其他节点
+
+# 8 sbin/start-all.sh
+
+# 9 运行示例
+# ./run-example org.apache.spark.examples.SparkPi spark://masterIP:7077
